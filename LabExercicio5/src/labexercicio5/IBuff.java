1e9974f431883e7fb6bc5a04cfe46f78783e1ba2
usuario <usuario@DESKTOP-8LIEST6>
2017-02-16 19:32:49 -0300

    Criado as assinaturas dos metodos

--- LabExercicio5/src/labexercicio5/IBuff.java
+++ LabExercicio5/src/labexercicio5/IBuff.java
@@ -1,5 +1,15 @@
 package labexercicio5;
 
-public interface IBuff {
+public interface IBuff
+{
+    public void adicionar();
+    
+    public void remover();
+    
+    public boolean estaVazio();
+    
+    public int numeroDeElementos();
+    
+    public int tamanhoMaximo();
     
 }

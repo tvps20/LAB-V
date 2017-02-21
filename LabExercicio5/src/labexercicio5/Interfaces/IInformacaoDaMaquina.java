a4a37fa527cef3af41521fe130fbcda410fa493a
Filipe Santiago <porto.santiago@hotmail.com>
2017-02-21 12:25:18 -0300

    Criando a interface IInformacaoDaMaquina

---
+++ LabExercicio5/src/labexercicio5/Interfaces/IInformacaoDaMaquina.java
@@ -0,0 +1,21 @@
+package labexercicio5.Interfaces;
+
+public interface IInformacaoDaMaquina 
+{
+    int getUsoRAM();
+
+    void setUsoRAM(int _usoRAM);
+
+    int getUsoHD();
+
+    void setUsoHD(int _usoHD);
+
+    int getUsoCPU();
+
+    void setUsoCPU(int _usoCPU);
+
+    double getClockCPU();
+
+    void setClockCPU(int _clockCPU);
+}
+}

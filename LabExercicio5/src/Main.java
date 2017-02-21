c04e145e90add38daf9dd0b3326a53931b524637
usuario <usuario@DESKTOP-8LIEST6>
2017-02-21 20:56:25 -0300

    Fazendo novos testes com relação ao metodo adicionar modificado

--- LabExercicio5/src/Main.java
+++ LabExercicio5/src/Main.java
@@ -9,16 +9,18 @@ public class Main {
         BufferCircular<IInformacaoDaMaquina> novoBuffe = new BufferCircular<IInformacaoDaMaquina>(4);
 
         InformacaoDaMaquina novaInformacao1 = new InformacaoDaMaquina();
-        InformacaoDaMaquina novaInformacao2 = new InformacaoDaMaquina();
-        InformacaoDaMaquina novaInformacao3 = new InformacaoDaMaquina();
-        InformacaoDaMaquina novaInformacao4 = new InformacaoDaMaquina();
-        InformacaoDaMaquina novaInformacao5 = new InformacaoDaMaquina();
+        //InformacaoDaMaquina novaInformacao2 = new InformacaoDaMaquina();
+        //InformacaoDaMaquina novaInformacao3 = new InformacaoDaMaquina();
+        //InformacaoDaMaquina novaInformacao4 = new InformacaoDaMaquina();
+        //InformacaoDaMaquina novaInformacao5 = new InformacaoDaMaquina();
 
         novoBuffe.adicionar(novaInformacao1);
-        novoBuffe.adicionar(novaInformacao2);
-        novoBuffe.adicionar(novaInformacao3);
-        novoBuffe.adicionar(novaInformacao4);
-        novoBuffe.adicionar(novaInformacao5);
+        //novoBuffe.adicionar(novaInformacao2);
+        //novoBuffe.adicionar(novaInformacao3);
+        //novoBuffe.adicionar(novaInformacao4);
+        //novoBuffe.adicionar(novaInformacao5);
+        
+        novoBuffe.remover();
     }
     
 }
